dcfbb1511c1d304b613adbe47d2168a19209d095
Asif Mohiuddin <mohiuddin.asif@gmail.com>
2021-06-16 09:47:53 +0200

    Updates

---
+++ bundling/entry-points/server.ts
@@ -0,0 +1,17 @@
+import server from "@frontity/core/src/server";
+import frontity__twentytwenty_theme_default from "@frontity/twentytwenty-theme/src/index";
+import frontity__wp_source_default from "@frontity/wp-source/src/index";
+import frontity__tiny_router_default from "@frontity/tiny-router/src/index";
+import frontity__html2react_default from "@frontity/html2react/src/index";
+import frontity__wp_comments_default from "@frontity/wp-comments/src/index";
+
+const packages = {
+  frontity__twentytwenty_theme_default,
+  frontity__wp_source_default,
+  frontity__tiny_router_default,
+  frontity__html2react_default,
+  frontity__wp_comments_default,
+};
+
+export default server({ packages });
+
